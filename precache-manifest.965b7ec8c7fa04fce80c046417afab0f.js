6f5d3bf565a0b6b2a63c21e146e8100633358114
Jakub <jakub_wozny_01@wp.pl>
2019-01-31 16:43:40 +0100

    Updates

---
+++ precache-manifest.965b7ec8c7fa04fce80c046417afab0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32da50d2cbc00da3973",
+    "url": "/binary-clock-app/static/js/runtime~main.c32da50d.js"
+  },
+  {
+    "revision": "d5778d9bff8319b67288",
+    "url": "/binary-clock-app/static/js/main.d5778d9b.chunk.js"
+  },
+  {
+    "revision": "dd6f2f63153005b4e7fe",
+    "url": "/binary-clock-app/static/js/1.dd6f2f63.chunk.js"
+  },
+  {
+    "revision": "d5778d9bff8319b67288",
+    "url": "/binary-clock-app/static/css/main.f2128610.chunk.css"
+  },
+  {
+    "revision": "da1e18668ce3d423dfbcdf4754f809cb",
+    "url": "/binary-clock-app/index.html"
+  }
+];
